123791f84e44120239cf1b019c59f2e4ab0aa8ad
bakdurak <lolwhat155@gmail.com>
2020-11-26 07:30:05 +0700

    3-feature-activity-news-screen: create domain object for article

--- app/src/main/java/com/example/rss_viewer/release/domain/ArticleDomain.kt
+++ app/src/main/java/com/example/rss_viewer/release/domain/ArticleDomain.kt
@@ -1,4 +1,15 @@
 package com.example.rss_viewer.release.domain
 
-class ArticleDomain {
+import android.graphics.Bitmap
+import com.prof.rssparser.Article
+
+class ArticleDomain(val imageUrl: String?, val title: String) {
+    companion object {
+        fun toDomain(article: Article) = ArticleDomain(article.image, article.title!!)
+    }
+
+    var srcBitMap: Bitmap? = null
+    var currentBitmap: Bitmap? = null
+
+    fun hasImage(): Boolean = imageUrl != null
 }
