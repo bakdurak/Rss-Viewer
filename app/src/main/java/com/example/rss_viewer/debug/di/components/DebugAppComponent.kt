2a92300440abf0a522b97f633acede5490b9c08b
bakdurak <lolwhat155@gmail.com>
2020-11-26 07:26:55 +0700

    3-feature-activity-news-screen: update debug package

--- app/src/main/java/com/example/rss_viewer/debug/di/components/DebugAppComponent.kt
+++ app/src/main/java/com/example/rss_viewer/debug/di/components/DebugAppComponent.kt
@@ -1,10 +1,14 @@
 package com.example.rss_viewer.debug.di.components
 
 import com.example.rss_viewer.debug.di.modules.FakeRepositoryModule
+import com.example.rss_viewer.debug.repositories.FakeNewsRepository
 import com.example.rss_viewer.release.di.components.AppComponent
+import com.example.rss_viewer.release.di.modules.NetworkModule
+import com.example.rss_viewer.release.di.modules.RepositoryModule
+import com.example.rss_viewer.release.di.modules.UtilsModule
 import dagger.Component
 import javax.inject.Singleton
 
-@Component(modules = [FakeRepositoryModule::class])
+@Component(modules = [FakeRepositoryModule::class, UtilsModule::class, NetworkModule::class])
 @Singleton
 interface DebugAppComponent : AppComponent {}
--- app/src/main/java/com/example/rss_viewer/debug/repositories/FakeNewsRepository.kt
+++ app/src/main/java/com/example/rss_viewer/debug/repositories/FakeNewsRepository.kt
@@ -1,12 +1,22 @@
 package com.example.rss_viewer.debug.repositories
 
+import com.example.rss_viewer.release.domain.ArticleDomain
 import com.example.rss_viewer.release.repositories.NewsRepository
-import com.prof.rssparser.Channel
+import com.example.rss_viewer.release.utils.ResponseState
 import io.reactivex.Observable
+import javax.inject.Inject
 
-class FakeNewsRepository : NewsRepository {
+class FakeNewsRepository @Inject constructor() : NewsRepository {
 
-    override fun getNewsByUrl(url: String): Observable<Channel> {
+    override fun getNewsByUrl(url: String, start: Int, count: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeNews(): Observable<ResponseState<List<ArticleDomain>>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDestroy() {
         TODO("Not yet implemented")
     }
 }
