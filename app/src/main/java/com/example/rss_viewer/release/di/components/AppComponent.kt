675f9d9ba637229f0eb335da3cb076c090a2577c
bakdurak <lolwhat155@gmail.com>
2020-11-26 07:28:36 +0700

    3-feature-activity-news-screen: update app component

--- app/src/main/java/com/example/rss_viewer/release/di/components/AppComponent.kt
+++ app/src/main/java/com/example/rss_viewer/release/di/components/AppComponent.kt
@@ -1,5 +1,6 @@
 package com.example.rss_viewer.release.di.components
 
+import com.example.rss_viewer.debug.repositories.FakeNewsRepository
 import com.example.rss_viewer.release.di.modules.NetworkModule
 import com.example.rss_viewer.release.di.modules.RepositoryModule
 import com.example.rss_viewer.release.di.modules.UtilsModule
@@ -13,6 +14,8 @@ import javax.inject.Singleton
 interface AppComponent  {
 
     fun inject(r: NewsRepositoryImpl)
+
+    fun inject(fr: FakeNewsRepository)
 
     fun inject(p: NewsPresenter)
 }
